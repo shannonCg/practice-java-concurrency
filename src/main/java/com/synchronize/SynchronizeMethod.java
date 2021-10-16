2ca19e93ea6b72ff5b3155053f1b8dd4e32f6d35
shannonCg <phy8233@gmail.com>
2021-10-16 13:50:25 +0800

    add synchronize practice

---
+++ src/main/java/com/synchronize/SynchronizeMethod.java
@@ -0,0 +1,45 @@
+package com.synchronize;
+
+import java.util.Date;
+
+public class SynchronizeMethod{
+
+    private String testName;
+
+    private static String TEST = "test";
+
+    public SynchronizeMethod(String testName){
+        this.testName = testName;
+    }
+
+    public String getTestName(){
+        return this.testName;
+    }
+
+    public synchronized void method1() {
+        try {
+            Thread.sleep(10*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(new Date()+":instance["+testName+"] finish method 1 ");
+    }
+    
+    public synchronized void method2() {
+        try {
+            Thread.sleep(10*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(new Date()+":instance["+testName+"] finish method 2 ");
+    }
+
+    public static synchronized void method3() {
+        try {
+            Thread.sleep(10*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(new Date()+":finish static method 3 ");
+    }
+}
